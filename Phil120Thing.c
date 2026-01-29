9a8a737ec667e2e891e43e9a283687cfb344a30a
Jenna Vervoort <vervoort@ualberta.ca>
2026-01-29 10:36:33 -0700

    made the menu work

--- Phil120Thing.c
+++ Phil120Thing.c
@@ -2,31 +2,34 @@
 #include <stdbool.h>
 
 void printGuide();
-int printMenu(); 
+int printMenu();
 
 int main(){
   bool gameOn = false;
   int action = printMenu(); 
+  
 
-  if(action == 1){
+  /*if(action == 1){
 
   }else if(action == 2){
     printGuide();
+
   }else {
     printf("*** GOODBYE ***");
-  }
+  }*/
 
   return 0;
 
-}
-
-int printMenu(){
-  int action = 0;
-  scanf("1. Start\n2. Guide\n3.Exit\nPlease select an action: %d\n", &action);
-  return action; 
 }
 
 void printGuide(){
 
 }
 
+int printMenu(){
+  int action = 0;
+  printf("*** Welcome ***\nTo The Truth Game\n--------------------\n");
+  printf("1. Start\n2. Guide\n3. Exit\nPlease select an action: ");
+  scanf("%d", &action);
+  return action;
+}
