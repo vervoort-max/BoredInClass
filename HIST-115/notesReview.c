84a603c7e9d2aeb7dba37b999ee3944eb305409e
Jenna Vervoort <vervoort@ualberta.ca>
2026-01-29 10:41:46 -0700

    Made new hist 115 program

---
+++ HIST-115/notesReview.c
@@ -0,0 +1,28 @@
+#include <stdio.h>
+
+//Gonna try to make a notes recall game for HIST 115 ???
+//Hopefully it will be good
+
+int printMenu();
+void reading(int unit);
+void flashcards(int unit);
+
+int main(){
+
+
+  return 0;
+}
+
+
+int printMenu(){
+
+  return 0;
+}
+
+void reading(int unit){
+
+}
+
+void flashcards(int unit){
+
+}
