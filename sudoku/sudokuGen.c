47c94012b6ada3b47161813bb0731784e8b60508
Jenna Vervoort <vervoort@ualberta.ca>
2026-03-09 01:16:26 -0600

    added more

--- sudoku/sudokuGen.c
+++ sudoku/sudokuGen.c
@@ -5,15 +5,33 @@
 int **emptyGrid();
 int checkRow(const int **grid, const int row, const int check);
 int checkCol(const int **grid, const int col, const int check);
-int validFill(const int **grid, const int fillVal);
+int validFill(const int **grid, const int fillVal, const int row, const int col);
 void fillBox(int **grid, const int fillVal);
-void printGrid(const int **grid);
+void printGrid(int **grid);
+void filler(const int **grid);
+void gen(int **grid, const char diff);
+void saveIt(int **grid);
 
 int main(int argc, char *argv){
 //Command line argument for difficulty
+	if(argc < 3){
+		printf("\nMust include 2 flags(ex. e p)");
+		exit(1);
+	}
+
 	srand(time(NULL));
 
 	int **grid = emptyGrid();
+	gen(grid, argv[1]);
+
+	switch(argv[2]){
+		case 'p':
+			printGrid(grid);
+			break;
+		case 's':
+			saveIt(grid);
+			break;
+	}
 
   return 0;
 }
@@ -37,15 +55,19 @@ int checkCol(const int **grid, const int col, const int check){
 	return 1;
 }
 
-int validFill(const int **grid, const int fillVal){
-
+int validFill(const int **grid, const int fillVal, const int row, const int col){
+	if(checkRow(grid, row, fillVal) && checkCol(grid, col, fillVal)){
+		return 1;
+	}else{
+		return 0;
+	}
 }
 
 void fillBox(int **grid, const int fillVal){
 
 }
 
-void printGrid(const int **grid){
+void printGrid(int **grid){
 	printf("\n--- --- --- --- --- --- --- --- ---\n");
 	for(int i = 0; i < 9; i++){
 		for(int j = 0; j < 9; j++){
@@ -56,5 +78,39 @@ void printGrid(const int **grid){
 }
 
 int **emptyGrid(){
+//Add malloc stuff here I guess
+
+	for(int i = 0; i < 9; i++){
+		for(int j = 0; j < 9; j++){
+
+		}
+	}
+}
+
+void gen(int **grid, const char diff){
+	int totFills = 0;
+	int filled = 0;
+	const int skip = rand();
+
+	switch(diff){
+		case 'e':
+			totFills = 60;
+			break;
+		case 'm':
+			totFills = 40;
+			break;
+		case 'h':
+			totFills = 17;
+			break;
+	}
+
+	while(filled != totFills){
+
+	}
+
+}
+
+
+void saveIt(int **grid){
 
 }
