935074d2c370682d4b9ebe069d0b22dfb4f2b569
Jenna Vervoort <vervoort@ualberta.ca>
2026-03-09 00:37:53 -0600

    fixing function prototypes

--- sudoku/sudokuGen.c
+++ sudoku/sudokuGen.c
@@ -2,39 +2,38 @@
 #include <stdlib.h>
 #include <time.h>
 
-void emptyGrid();
-int checkRow();
-int checkCol();
-int validFill();
-void fillBox();
-void printGrid(const int (*grid)[9]);
+int **emptyGrid();
+int checkRow(const int **grid);
+int checkCol(const int **grid);
+int validFill(const int **grid);
+void fillBox(int **grid);
+void printGrid(const int **grid);
 
 int main(int argc, char *argv){
 //Command line argument for difficulty
 	srand(time(NULL));
 
-	int (*grid)[9];
-	emptyGrid(&grid);
+	int **grid = emptyGrid();
 
   return 0;
 }
 
-int checkRow(){
+int checkRow(const int **grid){
 }
 
-int checkCol(){
-
-}
-
-int validFill(){
+int checkCol(const int **grid){
 
 }
 
-void fillBox(){
+int validFill(const int **grid){
 
 }
 
-void printGrid(const int (*grid)[9]){
+void fillBox(int **grid){
+
+}
+
+void printGrid(const int **grid){
 	for(int i = 0; i < 9; i++){
 		for(int j = 0; j < 9; j++){
 
@@ -42,10 +41,6 @@ void printGrid(const int (*grid)[9]){
 	}
 }
 
-void emptyGrid(int (*grid)[9]){
-	for(int i = 0; i < 9; i++){
-		for(int j = 0; j < 9; j++){
-			grid[i][j] = 0;
-		}
-	}
+int **emptyGrid(){
+
 }
